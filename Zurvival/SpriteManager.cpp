51e596900b91282c3612e5cfd55e41d607c48940
Dewcs <andreualien@hotmail.com>
2014-10-14 18:00:25 +0200

    Fix

--- Zurvival/SpriteManager.cpp
+++ Zurvival/SpriteManager.cpp
@@ -9,20 +9,42 @@ SpriteManager::~SpriteManager() {
 }
 
 void SpriteManager::addImage(SDL_Renderer *renderer, const char *fname, const std::string &key, SDL_Rect r) {
-	if (list.find(key) == list.end()) {
+	// check if key exists 
+	if (!keyExists(key)) {
 		SDL_Surface * image = IMG_Load(fname);
-		list[key] = SDL_CreateTextureFromSurface(renderer, image);
+		list[key] = Sprite(SDL_CreateTextureFromSurface(renderer, image),r);
 		SDL_FreeSurface(image);
+	}
+	else {
 	}
 }
 
-void SpriteManager::addText(SDL_Renderer *renderer, const char *text, const std::string &key, const SDL_Color &color, int ptsize, const char* fontfile) {
-	if (list.find(key) == list.end()) {
+void SpriteManager::addText(SDL_Renderer *renderer, const char *text, const std::string &key, const SDL_Color &color, int ptsize, const char* fontfile, SDL_Rect r) {
+	if (!keyExists(key)) {
 		TTF_Font* font;
 		font = TTF_OpenFont(fontfile, ptsize);
 		SDL_Surface *image = TTF_RenderText_Blended(font, text, color);
-		list[key] = SDL_CreateTextureFromSurface(renderer, image);
+		list[key] = Sprite(SDL_CreateTextureFromSurface(renderer, image), r);
 		SDL_FreeSurface(image);
 		TTF_CloseFont(font);
 	}
+	else {
+		std::cerr << "Repeated sprite key" << key << std::endl;
+	}
+}
+
+SDL_Texture* SpriteManager::getTexture(const std::string &key) {
+	return list[key].texture;
+}
+
+SDL_Rect SpriteManager::getRect(const std::string &key) {
+	return list[key].rect;
+}
+
+bool SpriteManager::keyExists(const std::string &key) {
+	return list.find(key) == list.end();
+}
+
+bool SpriteManager::fileExists(const char *fname) {
+
 }
--- Zurvival/SpriteManager.h
+++ Zurvival/SpriteManager.h
@@ -4,16 +4,36 @@
 #include <SDL_ttf.h>
 #include <map>
 #include <string>
+#include<iostream>
+
+struct Sprite {
+	SDL_Texture* texture;
+	SDL_Rect rect;
+	// create a new sprite with information
+	Sprite(SDL_Texture* texture, SDL_Rect rect) {
+		this->texture = texture;
+		this->rect = rect;
+	}
+	// fix for map
+	Sprite() {
+		texture = NULL;
+		rect = {};
+	}
+};
 
 class SpriteManager {
 	private:
-		std::map<std::string, SDL_Texture*> list;
+		std::map<std::string, Sprite> list;
 	public:
 		SpriteManager();
 		~SpriteManager();
 		void addImage(SDL_Renderer *renderer, const char *fname, const std::string &key, SDL_Rect r);
-		void addText(SDL_Renderer *renderer, const char *text, const std::string &key, const SDL_Color &color, int ptsize, const char* fontfile);
+		void addText(SDL_Renderer *renderer, const char *text, const std::string &key, const SDL_Color &color, int ptsize, const char* fontfile, SDL_Rect r);
+		SDL_Texture* getTexture(const std::string &key);
+		SDL_Rect getRect(const std::string &key);
 	private:
-		TTF_Font* loadfont(char* file, int ptsize);
+		bool keyExists(const std::string &key);
+		bool fileExists(const char *fname)
+		
 };
 
